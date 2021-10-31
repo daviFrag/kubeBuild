9cf84761d4635fbe72bc92aff5e66b2b553221ea
daviFrag <73591373+daviFrag@users.noreply.github.com>
2021-10-31 21:30:18 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -61,7 +61,7 @@ helm upgrade --install \
     
 helm upgrade --install \
     --set auth.username="minerva" \
-    --set auth.password="123456" \
+    --set auth.password="minerva" \
     --set extraConfiguration="default_vhost=myvhost" \
     "${KUBE_NAMESPACE}-rabbitmq" \
     bitnami/rabbitmq
