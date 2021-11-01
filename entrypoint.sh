651bd6eaed58e689957cad2e289b80bd7d0f4d45
daviFrag <73591373+daviFrag@users.noreply.github.com>
2021-11-01 10:53:41 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -62,7 +62,7 @@ helm upgrade --install \
 export check_rabbit=$(helm list --namespace=${KUBE_NAMESPACE} | grep -c rabbitmq)
 echo $check_rabbit
 
-if [ check_rabbit==0 ]; then
+if [ check_rabbit -eq 0 ]; then
     helm upgrade --install \
     --set auth.username="minerva" \
     --set auth.password="minerva" \
