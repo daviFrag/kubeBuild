eb49a7a8b9bf7cc3546fd42b58bcb1e21b93073e
daviFrag <davide.frag@gmail.com>
2022-01-26 21:13:31 +0100

    add redis in go-graph deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -100,6 +100,25 @@ if [ $TYPE == "go-graph" ]; then
         --set volumePermissions.enabled=true \
         "${KUBE_NAMESPACE}-postgresql" \
         bitnami/postgresql
+
+    helm install --name "${KUBE_NAMESPACE}-redis" \
+        --set cluster.slaveCount=1 \
+        --set securityContext.enabled=true \
+        --set securityContext.fsGroup=2000 \
+        --set securityContext.runAsUser=1000 \
+        --set volumePermissions.enabled=true \
+        --set master.persistence.enabled=true \
+        --set slave.persistence.enabled=true \
+        --set master.persistence.enabled=true \
+        --set master.persistence.path=/data \
+        --set master.persistence.size=8Gi \
+        --set master.persistence.storageClass=manual \
+        --set slave.persistence.enabled=true \
+        --set slave.persistence.path=/data \
+        --set slave.persistence.size=8Gi \
+        --set slave.persistence.storageClass=manual \
+        --namespace="$KUBE_NAMESPACE" \
+        stable/redis
 fi
 
 kubectl create secret \
